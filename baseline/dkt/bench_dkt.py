05754055e417747c3ebe21b126244312d2d5e4f4
Yahya Badran <techtweaking@gmail.com>
2024-03-08 20:30:43 +0100

    refactor DKT

--- baseline/dkt/bench_dkt.py
+++ baseline/dkt/bench_dkt.py
@@ -9,33 +9,19 @@ if __name__ == '__main__':
     @dataclass
     class Cfg:
         model_cls = DKT
-        #dataset_name = "AKT_assist2017"
-        #dataset_name = "assist2009"
-        #dataset_name = "dualingo2018_es_en"
-        #dataset_name = "corr_assist2009"
-        multi2one_kcs = True
-        window_size: int = 100
-        add_mask_label = False
-        add_teacher_mask = False
+        window_size: int = 150
         is_unfold = True
-        is_unfold_fixed_window = False
         all_in_one = True
-        #eval_method = Trainer.EVAL_UNFOLD_KC_LEVEL
+
         eval_method = Trainer.EVAL_UNFOLD_REDUCE
-
+        kfold = 5
         splits = [0.6, 0.5] 
-
 
     @dataclass
     class Traincfg:
         batch_size = 256
         eval_batch_size = 128
-        n_epoch = 10
+        n_epoch = 100
         lr = 0.001
     
-    #bench_model(Cfg(), Traincfg(), datasets = ['assist2009', 'corr_assist2009', 'dualingo2008_es_en'])
-    bench_model(Cfg(), Traincfg(), datasets = ['corr_assist2009'])
-    
-    
-
-    
+    bench_model(Cfg(), Traincfg(), datasets = ['assist2009', 'corr_assist2009', 'dualingo2008_es_en'])

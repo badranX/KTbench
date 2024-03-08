7154a222b8cea24644007559f6d1a62a71ce61d7
Yahya Badran <techtweaking@gmail.com>
2024-03-09 00:38:20 +0100

    fix duolingo dataset name typo

--- ktbench/datapipeline/middata_manager.py
+++ ktbench/datapipeline/middata_manager.py
@@ -24,7 +24,7 @@ dataset2gitbranch = {
     'AKT_assist2017': 'data/AKT_assist2017',
     'assist2009': 'data/assist2009',
     'corr_assist2009': 'data/corr_assist2009',
-    'dualingo2018_es_en': 'data/dualingo2018_es_en',
+    'duolingo2018_es_en': 'data/duolingo2018_es_en',
     'algebra2005': 'data/algebra2005'
 }
 

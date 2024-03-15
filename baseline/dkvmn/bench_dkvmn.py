b3c25797ff9662dfb644fab177cd549316d88e5d
Yahya Badran <techtweaking@gmail.com>
2024-03-15 16:30:02 +0100

    adjust mem models to correct hyperparams

--- baseline/dkvmn/bench_dkvmn.py
+++ baseline/dkvmn/bench_dkvmn.py
@@ -12,13 +12,13 @@ def main(datasets=['assist2009']):
         is_unfold = False
 
         eval_method = Pipeline.EVAL_QUESTION_LEVEL
-        kfold = 1
+        kfold = 5
 
     @dataclass
     class Traincfg:
         batch_size = 24
         eval_batch_size = 24
-        n_epoch = 2
+        n_epoch = 100
         lr = 0.001
     
     bench_model(Cfg(), Traincfg(), datasets = datasets)

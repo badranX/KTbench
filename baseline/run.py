2e96c36ecc34ad798fd337f9d4c64eb39421792a
Yahya Badran <yahya.badran@gmail.com>
2025-04-07 15:20:07 +0200

    simplify baseline/run script

--- baseline/run.py
+++ baseline/run.py
@@ -1,9 +1,9 @@
 #from baseline.akt import bench_akt_ml# bench_mask_label_akt
-from baseline.dkt import bench_dkt, bench_dkt_ad, bench_fuse_dkt, bench_dkt_ml
-from baseline.akt import bench_akt, bench_akt_ml, bench_akt_qm
-from baseline.qikt import bench_qikt
-from baseline.dkvmn import bench_dkvmn
-from baseline.deep_irt import bench_deep_irt
+from dkt import bench_dkt, bench_dkt_ad, bench_fuse_dkt, bench_dkt_ml
+from akt import bench_akt, bench_akt_ml, bench_akt_qm
+from qikt import bench_qikt
+from dkvmn import bench_dkvmn
+from deep_irt import bench_deep_irt
 
 datasets1 = ['assist2009', 'algebra2005']
 datasets2 = ['duolingo2018_es_en', 'corr_assist2009', 'riiid2020']
